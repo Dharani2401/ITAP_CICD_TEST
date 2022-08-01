39233703941cb1dd4a974a4e1781fef8b1244ff4
Dharani2401 <110018092+Dharani2401@users.noreply.github.com>
2022-08-01 17:56:14 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @echo off
 For /d /R %%G IN (conf.js) do (
 	IF EXIST %%G (
-		echo  Test Case Started %%G
+		echo  "Test Case Started" %%G
 		protractor %%G
-		echo Test Case Ended
+		echo "Test Case Ended"
 	)
 )
 	
