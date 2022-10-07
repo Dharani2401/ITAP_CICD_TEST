b8915cf4eab5da21e14f57f9fcdd5c37bd379367
Dharani2401 <110018092+Dharani2401@users.noreply.github.com>
2022-10-07 08:47:22 +0530

    Update spec.js

--- xyz_demo/login-dep_data0/spec.js
+++ xyz_demo/login-dep_data0/spec.js
@@ -6,7 +6,7 @@
               const Get_Attribute = new Map();
               describe('xyz_demo', function() {
                     it('login-dep_data0', function() {
-                        console.log('Application_name : xyz_demo, Scenario_name : xyz_demo\login-dep_data0');
+                        console.log('Application_name : xyz_demo, Scenario_name : login-dep_data0');
                         browser.ignoreSynchronization =true;
                         browser.driver.manage().window().maximize();
                         browser.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
@@ -31,7 +31,7 @@
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(2)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page1.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -41,7 +41,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//div')).get(4)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page2.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -51,7 +51,7 @@ browser.sleep(500);
 browser.sleep(3000);
 browser.wait(until.presenceOf(element(by.xpath('//select[@name="userSelect"]//option[@value="2"]'))), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page3.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -61,7 +61,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(2)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page4.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -71,7 +71,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(3)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page5.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -81,7 +81,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//div')).get(10)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page6.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -92,7 +92,7 @@ browser.sleep(3000);
 browser.wait(until.presenceOf(element(by.xpath('//input'))), delay, 'Element taking too long to appear in the DOM');
 element(by.xpath('//input')).clear();
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page7.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -101,7 +101,7 @@ element(by.xpath('//input')).sendKeys("1000");
 
 browser.wait(until.presenceOf(element.all(by.xpath('//div')).get(4)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page8.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -111,7 +111,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(5)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page9.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -121,7 +121,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(1)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page10.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
@@ -131,7 +131,7 @@ browser.sleep(500);
 
 browser.wait(until.presenceOf(element.all(by.xpath('//button')).get(0)), delay, 'Element taking too long to appear in the DOM');
 browser.takeScreenshot().then(function (png) {
-                        var dir="xyz_demo\login-dep_data0";
+                        var dir="xyz_demo\\login-dep_data0";
                               var fname="page11.png"; var stream1 = fs.createWriteStream(path.join(dir, fname));
             stream1.write(new Buffer(png, 'base64'));
             stream1.end();
